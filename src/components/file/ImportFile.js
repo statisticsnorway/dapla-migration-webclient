bf55ede0ebe3bd54e5b9404ad32ab38f59e77d98
mmj-ssb <mmj@ssb.no>
2021-04-13 19:11:44 +0200

    View file content

--- src/components/file/ImportFile.js
+++ src/components/file/ImportFile.js
@@ -1,5 +1,5 @@
 import useAxios from 'axios-hooks'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import {
   Button,
@@ -18,6 +18,7 @@ import {
 import { v4 as uuidv4 } from 'uuid'
 
 import ImportFileStatus from './ImportFileStatus'
+import { APP_STEPS } from '../../configurations'
 
 const typeOptions = [
   { key: 'String', text: 'String', value: 'String' },
@@ -98,6 +99,9 @@ function ImportFile () {
 
   return (
     <Segment basic>
+      <Link to={APP_STEPS[1].route}>
+        <Icon size="large" color="blue" name="arrow left" />
+      </Link>
       <Header size="large" content={data.files[0]} />
       <Divider hidden />
       <Grid columns="equal">
